9897941a98899258728c1a2427e54c00e0af67dd
Florian Schwarb <schwarbf@student.ethz.ch>
2023-05-07 12:16:26 +0200

    changed to rectangular confidence regions

--- src/getpval.R
+++ src/getpval.R
@@ -2,7 +2,7 @@
 #'
 #' @description Tests whether a given subset of predictors satisfies the 
 #'    hypothesis of 'invariance under random effects' without using subenvironments.
-#'
+#'su
 #' @param X data.frame: Contains the fixed-effects predictor.
 #' @param Y vector: Contains the response value for all environments.
 #' @param ExpInd list: Contains the indicators which environment observation 
@@ -38,76 +38,65 @@ getpval <- function (X, Y, ExpInd, alpha = 0.05, test = "lme4") {
   if("intercept" %in% colnames(X) && ncol(X) > 1) {confInt <- confInt[-1, , drop = FALSE]}
   
   
-  # perform hypothesis test for one against the rest
-  envs <- 1 # change to 'envs <- unique(ExpInd)' to do all against all
+  # use Bonferroni-correction
+  alphaBonfCor <- alpha/ncol(X)
+  indEnv <- which(ExpInd == 1)
   notRejected <- TRUE
-  i <- 1
-  alphaBonfCor <- alpha/length(envs)
-  while(notRejected & i <=length(envs)) {
-    env <- envs[i]
-    indEnv <- which(ExpInd == env)
     
-    # check if matrix data[indEnv, ] has full column rank 
-    if(!("intercept" %in% colnames(X))) {
-      dataTest <- data[indEnv, , drop = FALSE]
-      columnSums <- colSums(dataTest)
-      colRankTest <- sapply(2:ncol(data), 
-                            function(i) columnSums[i]/nrow(dataTest) == dataTest[1, i]) %>% sum()
-      if(colRankTest == (ncol(data) - 1)) {
-        stop(paste0("The matrix of predictors in environment ", env, " has not full ", 
-                    "column rank. Did you apply a do-intervention?"))
-      }
-    } 
+  # check if matrix data[indEnv, ] has full column rank 
+  if(!("intercept" %in% colnames(X))) {
+    dataTest <- data[indEnv, , drop = FALSE]
+    columnSums <- colSums(dataTest)
+    colRankTest <- sapply(2:ncol(data), 
+                          function(i) columnSums[i]/nrow(dataTest) == dataTest[1, i]) %>% sum()
+    if(colRankTest == (ncol(data) - 1)) {
+      stop(paste0("The matrix of predictors in environment ", 1, " has not full ", 
+                  "column rank. Did you apply a do-intervention?"))
+    }
+  } 
 
-    # estimate 'beta + b' on single environment
-    formLM <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), " -1 "))
-    lm.fit <- lm(formLM, data = data[indEnv, , drop = FALSE])
-    confInt_beta_lm <- confint(lm.fit, alpha = alphaBonfCor/2)
-    colnames(confInt_beta_lm) <- c("lower", "upper")
-    
-    # estimate tau on rest of environments
-    if(test == "lme4"){
-      ExpIndTest <- ExpInd[-which(ExpInd == env)]
-      formLMMTest <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), 
-                                      " -1 +", paste0("(", paste0(cols[-1], collapse= " + ")), 
-                                      "-1 | ExpIndTest)"))
-      lmer.fit <- lmer(formLMMTest, data = data[-indEnv, ], REML = TRUE) %>% 
-        suppressMessages()
-      # confInt_beta <- confint(lmer.fit, parm = "beta_", method = "Wald",
-      #                         level = 1 - alphaBonfCor/2) %>% suppressMessages()
-      confInt_beta_lmm <- confint(lmer.fit, parm = "beta_", method = "Wald",
-                                  level = 1 - alphaBonfCor/2) %>% suppressMessages()
-      colnames(confInt_beta_lmm) <- c("lower", "upper")
-      # tauHat <- VarCorr(lmer.fit)$ExpIndTest[1, 1] %>% sqrt()
-    } else if(test == "nmle") {
-      ExpIndTest <<- ExpInd[-which(ExpInd == env)]
-      formLMMTest <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), " -1"))
-      formFxdTest <- as.formula(paste(" ~ ", paste(cols[-1], collapse= "+"), " -1 | ExpIndTest"))
-      formRdmTest <- as.formula(paste("~", paste(cols[-1], collapse= "+"), " -1"))
-      varFixedTest <- varIdent(form = formFxdTest)
-      # lme.fit <- lmeFit(formLMMTest, random = list(ExpIndTest = pdIdent(formRdmTest)),
-      #                   weights = varFixedTest, data = data[-indEnv, ], nrestarts = 5)
-      lme.fit <- lmeFit(formLMMTest, random = list(ExpIndTest = pdIdent(formRdmTest)),
-                        weights = NULL, data = data[-indEnv, ], nrestarts = 5)
-      confInt_beta_lmm <- intervals(lme.fit, which = "fixed", level = 1- alphaBonfCor/2)$fixed %>%
-        as.data.frame() %>%
-        select(c("lower", "upper"))
-      rm(ExpIndTest, envir = .GlobalEnv)
-    } else{
-      stop(paste0("Test ", test, " is currently not implemented."))
-    }
-    
-    checkOverlap <- function(i) {
-      confInt_beta_lm[i, "upper"] < confInt_beta_lmm[i, "lower"] ||
-        confInt_beta_lmm[i, "upper"] < confInt_beta_lm[i, "lower"]
-    }
-    checkOverlapOut <- sapply(1:nrow(confInt_beta_lm), checkOverlap) %>% sum()
-    if(checkOverlapOut > 0) {notRejected <- FALSE}
-    i <- i + 1
+  # estimate 'beta + b' on single environment
+  formLM <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), " -1 "))
+  lm.fit <- lm(formLM, data = data[indEnv, , drop = FALSE])
+  confInt_beta_lm <- confint(lm.fit, alpha = alphaBonfCor/2)
+  colnames(confInt_beta_lm) <- c("lower", "upper")
+  
+  # estimate tau on rest of environments
+  if(test == "lme4"){
+    ExpIndTest <- ExpInd[-which(ExpInd == 1)]
+    formLMMTest <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), 
+                                    " -1 +", paste0("(", paste0(cols[-1], collapse= " + ")), 
+                                    "-1 | ExpIndTest)"))
+    lmer.fit <- lmer(formLMMTest, data = data[-indEnv, ], REML = TRUE) %>% 
+      suppressMessages()
+    confInt_beta_lmm <- confint(lmer.fit, parm = "beta_", method = "Wald",
+                                level = 1 - alphaBonfCor/2) %>% suppressMessages()
+    colnames(confInt_beta_lmm) <- c("lower", "upper")
+  } else if(test == "nmle") {
+    ExpIndTest <<- ExpInd[-which(ExpInd == 1)]
+    formLMMTest <- as.formula(paste("Y ~ ", paste(cols[-1], collapse= "+"), " -1"))
+    formFxdTest <- as.formula(paste(" ~ ", paste(cols[-1], collapse= "+"), " -1 | ExpIndTest"))
+    formRdmTest <- as.formula(paste("~", paste(cols[-1], collapse= "+"), " -1"))
+    varFixedTest <- varIdent(form = formFxdTest)
+    lme.fit <- lmeFit(formLMMTest, random = list(ExpIndTest = pdIdent(formRdmTest)),
+                      weights = NULL, data = data[-indEnv, ], nrestarts = 5)
+    confInt_beta_lmm <- intervals(lme.fit, which = "fixed", level = 1- alphaBonfCor/2)$fixed %>%
+      as.data.frame() %>%
+      select(c("lower", "upper"))
+    rm(ExpIndTest, envir = .GlobalEnv)
+  } else{
+    stop(paste0("Test ", test, " is currently not implemented."))
   }
+  
+  checkOverlap <- function(i) {
+    confInt_beta_lm[i, "upper"] < confInt_beta_lmm[i, "lower"] ||
+      confInt_beta_lmm[i, "upper"] < confInt_beta_lm[i, "lower"]
+  }
+  checkOverlapOut <- sapply(1:nrow(confInt_beta_lm), checkOverlap) %>% sum()
+  if(checkOverlapOut > 0) {notRejected <- FALSE}
   result <- if(notRejected) {"not rejected"} else{"rejected"}
   
-  # construction of confidence interval
+  # construction of confidence regions
   if(result == "not rejected") {
     getConfInt <- function(i) {
       c(max(confInt_beta_lm[i, "lower"], confInt_beta_lmm[i, "lower"]), 
